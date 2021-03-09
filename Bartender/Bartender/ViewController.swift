71e0a496422fa1c0c872b317f4360df39507d558
Keo Shiko <keoshiko@gmail.com>
2021-03-09 09:52:22 +0200

    Created the project

---
+++ Bartender/Bartender/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bartender
+//
+//  Created by Keo Shiko on 2021/03/09.
+//  Copyright © 2021 Glucode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
