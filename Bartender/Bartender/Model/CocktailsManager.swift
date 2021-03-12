7de5eb38ab8d2e0bdd9b6c59f2b8f5fb8a6c5b34
Keo Shiko <keoshiko@glucode.com>
2021-03-12 15:21:54 +0200

    R4 - debugging the output

--- Bartender/Bartender/Model/CocktailsManager.swift
+++ Bartender/Bartender/Model/CocktailsManager.swift
@@ -10,6 +10,8 @@ import Foundation
 
 struct CocktailsManager {
     
+    //var categories : CocktailData
+    
     let categoryURL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
     
     func performRequest(with urlString : String) {
@@ -28,8 +30,9 @@ struct CocktailsManager {
                 if let safeData = data {
                     
                     if let category = self.parseJson(cocktailData: safeData) {
-                        print("yay")
-                        print(category)
+                        
+                        print(category.drinks.count)
+                        
                     }
                 }
             }
@@ -38,14 +41,14 @@ struct CocktailsManager {
         }
     }
     
-    func parseJson(cocktailData : Data) -> CocktailData? {
+     func parseJson(cocktailData : Data) -> CocktailData? {
         
         let decoder = JSONDecoder()
         
         do {
             let decodedData = try decoder.decode(CocktailData.self, from: cocktailData)
             
-            print(decodedData.drinks[0])
+            
             
             return decodedData
         }
@@ -53,4 +56,53 @@ struct CocktailsManager {
             return nil
         }
     }
+    
+    mutating func setCategories(category : CocktailData) {
+        //categories = category
+    }
+    
+    func performDrinksRequest(urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                
+                if error != nil {
+                    print(error!.localizedDescription)
+                    return
+                }
+                
+                if let safeData = data {
+                    
+                    if let drinks = self.parseJson(cocktailData: safeData) {
+                        
+                        print(drinks)
+                        
+                        
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+     func parseDrinksJson(cocktailData : Data) -> CocktailDetails? {
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(CocktailDetails.self, from: cocktailData)
+            
+            print("yeyi")
+            
+            return decodedData
+        }
+        catch {
+            return nil
+        }
+    }
+    
 }
