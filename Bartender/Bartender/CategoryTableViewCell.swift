1b5f6d8155d69a96ed0ece057463e36d74d757e7
Keo Shiko <keoshiko@glucode.com>
2021-03-15 10:32:31 +0200

    R4 - created category cell class

---
+++ Bartender/Bartender/CategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewCell.swift
+//  Bartender
+//
+//  Created by Keo Shiko on 2021/03/15.
+//  Copyright © 2021 Glucode. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
