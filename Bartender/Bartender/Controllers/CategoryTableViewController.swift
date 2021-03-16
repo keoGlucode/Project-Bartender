bfee0e3683e1a2aeafbce7b6c975c696fc3b84f1
Keo Shiko <keoshiko@glucode.com>
2021-03-16 11:28:17 +0200

    R4 - Specified number of rows for the second table view

--- Bartender/Bartender/Controllers/CategoryTableViewController.swift
+++ Bartender/Bartender/Controllers/CategoryTableViewController.swift
@@ -38,6 +38,12 @@ class CategoryTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let title = cocktails.catergories.drinks[indexPath.row]
+    }
+    
     
 }
 
