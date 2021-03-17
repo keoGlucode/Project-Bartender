43761a308caedc732e381edf9e5e82acb77d0091
Keo Shiko <keoshiko@glucode.com>
2021-03-17 13:07:05 +0200

    R5 - Implemented the didSelectRowAt function with its perform segue

--- Bartender/Bartender/Controllers/CocktailsTableViewController.swift
+++ Bartender/Bartender/Controllers/CocktailsTableViewController.swift
@@ -57,6 +57,10 @@ class CocktailsTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "viewDrinksDetails", sender: self)
+    }
+    
    /* override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
