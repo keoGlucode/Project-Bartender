433361857282c1bd910011ec366ae09f4043ac73
Keo Shiko <keoshiko@glucode.com>
2021-03-18 21:01:23 +0200

    R5 - debugging, changing url to match JSON

--- Bartender/Bartender/Controllers/CocktailsTableViewController.swift
+++ Bartender/Bartender/Controllers/CocktailsTableViewController.swift
@@ -58,7 +58,14 @@ class CocktailsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "viewDrinksDetails", sender: self)
+        //performSegue(withIdentifier: "viewDrinksDetails", sender: self)
+        
+        let vc = storyboard?.instantiateViewController(identifier: "DrinkAttributesViewController") as! DrinkAttributesViewController
+        
+        //vc.drinkID = cocktailItem.drinks.drinks[indexPath.row].idDrink
+        
+        
+        self.navigationController?.pushViewController(vc, animated: true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -67,71 +74,13 @@ class CocktailsTableViewController: UITableViewController {
             
             let drink = cocktailItem.drinks.drinks[(tableView.indexPathForSelectedRow?.row)!]
             
+            destination.drinkImage.loadImages(urlString: drink.strDrinkThumb)
             
+            destination.drinkLabel.text = drink.strDrink
         }
     }
-    
-   /* override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        let title = cocktailItem.drinks.drinks[indexPath.row]
-        
 
-    }*/
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
