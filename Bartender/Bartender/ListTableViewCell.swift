0c10eecedfc921f2c9e5f1312d7e9c5502e438b6
Keo Shiko <keoshiko@glucode.com>
2021-03-23 15:25:25 +0200

    R4 - Created table view for list

---
+++ Bartender/Bartender/ListTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ListTableViewCell.swift
+//  Bartender
+//
+//  Created by Keo Shiko on 2021/03/23.
+//  Copyright © 2021 Glucode. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBOutlet weak var listLabel: UILabel!
+    
+    @IBOutlet weak var listImage: UIImageView!
+    
+}
