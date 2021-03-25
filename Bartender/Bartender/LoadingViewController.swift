1b99ed8b41b1c4030823203754720ec96ee6faeb
Keo Shiko <keoshiko@glucode.com>
2021-03-25 16:36:56 +0200

    R6 - Created loading view controller file

---
+++ Bartender/Bartender/LoadingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoadingViewController.swift
+//  Bartender
+//
+//  Created by Keo Shiko on 2021/03/25.
+//  Copyright © 2021 Glucode. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
